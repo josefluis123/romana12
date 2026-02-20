17924610e8e2061f0bbde3ea6b6d82769f340552
Kirk Besa <kirkpatrickbesa@gmail.com>
2026-02-21 01:39:24 +0800

    adjust nav gaps + responsive gaps

--- src/pages/kirk/_components/NavBarReact.tsx
+++ src/pages/kirk/_components/NavBarReact.tsx
@@ -7,7 +7,7 @@ const NavBarReact = ({ currentPath }: { currentPath: string }) => {
 
   return (
     <nav className="fixed top-0 left-0 w-full z-50 flex items-center justify-center p-8">
-      <div className="w-full flex items-center justify-between px-6 py-2 rounded-3xl shadow-lg border border-white/10 bg-white/5 backdrop-blur-md">
+      <div className="w-full flex items-center justify-between md:px-12 px-6 py-2 rounded-3xl shadow-lg border border-white/10 bg-white/5 backdrop-blur-md">
         <a href="/kirk">
           <img
             src="/kirk/nav-logo.png"
@@ -17,7 +17,7 @@ const NavBarReact = ({ currentPath }: { currentPath: string }) => {
           />
         </a>
 
-        <ul className="h-fit flex items-center justify-center gap-6 py-4">
+        <ul className="h-fit flex items-center justify-center gap-6 md:gap-12 py-4">
           {NAV_LINKS.map((link, index) => {
             const isActive = currentPath === link.href;
 
